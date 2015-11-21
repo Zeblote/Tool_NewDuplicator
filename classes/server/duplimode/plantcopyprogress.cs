bf63d8dabb7db0945964d818611615165c9f3424
Zeblote <zeblote@gmail.com>
2015-11-21 23:52:40 +0100

    Show percentage instead of count for planting

--- classes/server/duplimode/plantcopyprogress.cs
+++ classes/server/duplimode/plantcopyprogress.cs
@@ -65,13 +65,17 @@ function NDM_PlantCopyProgress::getBottomPrint(%this, %client)
 	{
 		//Searching for a brick
 		%pIndex = %client.ndSelection.plantSearchIndex;
-		%title = "Finding Next Brick... (\c3" @ %pIndex @ "\c6 / \c3" @ %count @ "\c6, \c3" @ %planted @ "\c6 planted)";
+		%percent = mFloor(%client.ndSelection.plantSearchIndex * 100 / %count);
+
+		%title = "Finding Next Brick... (\c3" @ %percent @ "%\c6, \c3" @ %planted @ "\c6 planted)";
 	}
 	else
 	{
 		//Planting bricks
 		%failed = %client.ndSelection.plantTrustFailCount + %client.ndSelection.plantBlockedFailCount;
-		%title = "Planting... (\c3" @ %planted @ "\c6 Bricks, \c3" @ %failed @ "\c6 failed)";
+		%percent = mFloor(%planted * 100 / %count);
+
+		%title = "Planting... (\c3" @ %percent @ "%\c6, \c3" @ %failed @ "\c6 failed)";
 	}
 
 	%l0 = "[Cancel Brick]: Cancel planting";
