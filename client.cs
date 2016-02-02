081e11565e84032f21cdf648557ef2cedaa68b06
Zeblote <zeblote@gmail.com>
2016-02-02 23:33:14 +0100

    Increase version number

--- client.cs
+++ client.cs
@@ -8,7 +8,7 @@
 // *
 // * ######################################################################
 
-$ND::Version = "1.3.1";
+$ND::Version = "1.3.2";
 
 $ND::FilePath = filePath($Con::File) @ "/";
 $ND::ConfigPath = "config/NewDuplicator/";
