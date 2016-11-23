bbdf2e832ad9c4ef48e91eaead78aa141ed6bbd8
Zeblote <zeblote@gmail.com>
2016-11-23 22:12:34 +0100

    Add option to plant bricks in diff group

--- server.cs
+++ server.cs
@@ -1,7 +1,7 @@
 // Executes all required scripts and initializes the server side.
 // -------------------------------------------------------------------
 
-$ND::Version = "1.5.2";
+$ND::Version = "1.6.0";
 
 $ND::FilePath = filePath($Con::File) @ "/";
 $ND::ConfigPath = "config/NewDuplicator/";
